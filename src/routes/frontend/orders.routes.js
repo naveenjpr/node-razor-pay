2b715b6782ebf7928731de9fd143a3615aa871d8
naveensainijpr@gmail.com <naveensainijpr@gmail.com>
2025-03-29 21:41:48 +0530

    first

--- src/routes/frontend/orders.routes.js
+++ src/routes/frontend/orders.routes.js
@@ -4,8 +4,8 @@ const orderController = require('../../controllers/frontend/orders.controller');
 
 module.exports = app => {
 
-    route.get('/place-order', orderController.placeOrder);
-    route.get('/confirm-order', orderController.confirmOrder);
+    route.post('/place-order', orderController.placeOrder);
+    route.post('/confirm-order', orderController.confirmOrder);
 
     app.use('/api/frontend/orders', route);
 
